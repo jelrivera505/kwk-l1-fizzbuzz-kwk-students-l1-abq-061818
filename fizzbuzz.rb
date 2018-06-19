9e90878821e5ff9826721ef8cdb593bba9f6ce12
jelrivera505 <jelrivera505@gmail.com>
2018-06-19 20:03:11 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+def fizzbuzz(x) == "fizz"
+  fizzbuzz_3 == "fizz"
+end 
+
+def fizzbuzz(y) == "buzz"
+  fizzbuzz_5 == "buzz"
+end 
+
+def fizzbuzz(z) == "fizzbuzz"
+  fizzbuzz_15 == "fizzbuzz"
+end 
