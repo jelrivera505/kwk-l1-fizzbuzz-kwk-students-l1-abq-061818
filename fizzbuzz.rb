250b6b2935b172bf884ddd8330dee323e165e67c
jelrivera505 <jelrivera505@gmail.com>
2018-06-19 20:10:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,10 @@
-def fizzbuzz(x) == "fizz"
+
+if fizzbuzz(x) == "fizz"
   fizzbuzz_3 == "fizz"
-end 
 
-def fizzbuzz(y) == "buzz"
+elsif fizzbuzz(y) == "buzz"
   fizzbuzz_5 == "buzz"
-end 
 
-def fizzbuzz(z) == "fizzbuzz"
+else fizzbuzz(z) == "fizzbuzz"
   fizzbuzz_15 == "fizzbuzz"
 end 
